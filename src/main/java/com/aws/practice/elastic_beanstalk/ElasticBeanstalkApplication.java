9fa46af49854f27111a338702047c137ac578ed0
sistatress <sistatress971@gmail.com>
2025-11-01 16:43:08 +0100

    Initialized project.

---
+++ src/main/java/com/aws/practice/elastic_beanstalk/ElasticBeanstalkApplication.java
@@ -0,0 +1,13 @@
+package com.aws.practice.elastic_beanstalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElasticBeanstalkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElasticBeanstalkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aws/practice/elastic_beanstalk/ElasticBeanstalkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aws.practice.elastic_beanstalk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElasticBeanstalkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
